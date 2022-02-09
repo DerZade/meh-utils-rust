93d579be73206d0d7f4c4aa3b861d060dfb86da6
Moritz Schmidt <fusselwurm@gmail.com>
2022-02-09 23:10:51 +0100

    parametrize a test using rstest; fix points on periphery

--- src/mvt/clip_feature.rs
+++ src/mvt/clip_feature.rs
@@ -1,19 +1,23 @@
-use geo::contains::Contains;
-use geo::{Coordinate, GeoFloat, Geometry, Line, Point, Rect};
+use geo::{Coordinate, GeoFloat, Geometry, Line, Point, Polygon, Rect};
 use geo::algorithm::line_intersection::{line_intersection, LineIntersection};
 use geo::algorithm::euclidean_distance::EuclideanDistance;
 
 #[cfg(test)]
 mod tests {
-    use geo::{Coordinate, Line, Point, Rect};
+    use rstest::rstest;
+    use geo::{Coordinate, Line, LineString, Point, Polygon, Rect};
     use crate::mvt::clip_feature::Clip;
+
+    fn box_0_0_to_5_10() -> Rect<f64> {
+        Rect::new(
+            Coordinate {x: 0.0, y: 0.0},
+            Coordinate {x: 5.0, y: 10.0},
+        )
+    }
 
     #[test]
     fn clip_point_returns_none_if_point_outside_of_box() {
-        let rect = Rect::new(
-            Coordinate {x: 0.0, y: 0.0},
-            Coordinate {x: 5.0, y: 10.0},
-        );
+        let rect = box_0_0_to_5_10();
         let point = geo::Geometry::Point(Point(Coordinate {x: 6.0, y: 5.0}));
 
         let clipped  = point.clip(&rect);
@@ -21,13 +25,13 @@ mod tests {
         assert!(clipped.is_none());
     }
 
-    #[test]
-    fn clip_point_returns_point_if_point_inside_of_box() {
-        let rect = Rect::new(
-            Coordinate {x: 0.0, y: 0.0},
-            Coordinate {x: 5.0, y: 10.0},
-        );
-        let point = geo::Geometry::Point(Point(Coordinate {x: 1.0, y: 5.0}));
+    #[rstest]
+    #[case(0.0, 0.0)]
+    #[case(1.0, 5.0)]
+    #[case(5.0, 10.0)]
+    fn clip_point_returns_point_if_point_inside_of_box(#[case] x: f64, #[case] y: f64) {
+        let rect = box_0_0_to_5_10();
+        let point = geo::Geometry::Point(Point(Coordinate {x, y}));
 
         let clipped = point.clip(&rect);
 
@@ -37,10 +41,7 @@ mod tests {
 
     #[test]
     fn clip_line_returns_none_if_linestring_outside_of_box() {
-        let rect = Rect::new(
-            Coordinate {x: 0.0, y: 0.0},
-            Coordinate {x: 5.0, y: 10.0},
-        );
+        let rect = box_0_0_to_5_10();
 
         let line = geo::Geometry::Line(Line::new(
             Coordinate {x: 4.0, y: -3.0},
@@ -54,10 +55,7 @@ mod tests {
 
     #[test]
     fn clip_line_returns_complete_line_if_line_inside_of_box() {
-        let rect = Rect::new(
-            Coordinate {x: 0.0, y: 0.0},
-            Coordinate {x: 5.0, y: 10.0},
-        );
+        let rect = box_0_0_to_5_10();
 
         let line = geo::Geometry::Line(Line::new(
             Coordinate {x: 1.0, y: 1.0},
@@ -72,10 +70,7 @@ mod tests {
 
     #[test]
     fn clip_line_returns_complete_line_if_line_on_edge_of_box() {
-        let rect = Rect::new(
-            Coordinate {x: 0.0, y: 0.0},
-            Coordinate {x: 5.0, y: 10.0},
-        );
+        let rect = box_0_0_to_5_10();
 
         let line = geo::Geometry::Line(Line::new(
             Coordinate {x: 0.0, y: 0.0},
@@ -90,10 +85,7 @@ mod tests {
 
     #[test]
     fn clip_line_returns_clipped_line_if_line_passes_through_box() {
-        let rect = Rect::new(
-            Coordinate {x: 0.0, y: 0.0},
-            Coordinate {x: 5.0, y: 10.0},
-        );
+        let rect = box_0_0_to_5_10();
 
         let line = geo::Geometry::Line(Line::new(
             Coordinate {x: -2.5, y: 0.0},
@@ -128,10 +120,7 @@ mod tests {
 
     #[test]
     fn clip_line_returns_clipped_line_if_line_starts_within_but_goes_outside_the_box() {
-        let rect = Rect::new(
-            Coordinate {x: 0.0, y: 0.0},
-            Coordinate {x: 5.0, y: 10.0},
-        );
+        let rect = box_0_0_to_5_10();
 
         let line = geo::Geometry::Line(Line::new(
             Coordinate {x: 2.5, y: 5.0},
@@ -165,6 +154,26 @@ mod tests {
         }
     }
 
+
+    #[test]
+    fn clip_polygon_that_surrounds_box_will_return_box() {
+        let rect = box_0_0_to_5_10();
+
+        let polygon = geo::Geometry::Polygon(Polygon::new(
+            LineString(vec![
+                Coordinate {x: -1.0, y: -1.0},
+                Coordinate {x: -1.0, y: 11.0},
+                Coordinate {x: 6.0, y: 11.0},
+                Coordinate {x: 6.0, y: -1.0},
+            ]),
+            vec![],
+        ));
+
+        let clipped = polygon.clip(&rect);
+
+        assert!(clipped.is_some());
+        assert_eq!(clipped.unwrap(),geo::Geometry::Polygon(rect.to_polygon()));
+    }
 }
 
 // it would be neat to generalize this to a Diff trait (subtract one geometry from another!)
@@ -180,6 +189,7 @@ impl<T: GeoFloat> Clip<T> for Geometry<T> {
         match self {
             Geometry::Point(pt) => pt.clip(rect).map(|p| {Geometry::Point(p)}),
             Geometry::Line(l) => l.clip(rect).map(|l| {Geometry::Line(l)}),
+            Geometry::Polygon(pg) => pg.clip(rect).map(|pg| {Geometry::Polygon(pg)}),
             _ => None,
         }
     }
@@ -251,6 +261,22 @@ impl<T: GeoFloat> Clip<T> for Point<T> {
     type Output = Point<T>;
 
     fn clip(&self, rect: &Rect<T>) -> Option<Self::Output> {
-        rect.contains(self).then(|| {self.clone()})
+        let (px, py) = self.x_y();
+        let (rx1, ry1) = rect.min().x_y();
+        let (rx2, ry2) = rect.max().x_y();
+
+        if (px >= rx1) && (px <= rx2) && (py >= ry1) && (py <= ry2) {
+            Some(self.clone())
+        } else {
+            None
+        }
+    }
+}
+
+impl<T: GeoFloat> Clip<T> for Polygon<T> {
+    type Output = Polygon<T>;
+
+    fn clip(&self, rect: &Rect<T>) -> Option<Self::Output> {
+        Some(rect.clone().to_polygon())
     }
 }
